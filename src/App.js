94355685e75fda717023e11c8ffe02f3564588fe
arifphero <ahammadarif232@gmail.com>
2022-04-28 23:12:04 +0600

    text updated

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="App">
       <h1>creating warehouse related project</h1>
+      <p>and i have to finish this project within tomorrow night</p>
     </div>
   );
 }
