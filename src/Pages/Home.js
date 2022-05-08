87df6f5627bd52e4b6fb8c2d8de3460b740e1e9a
arifphero <ahammadarif232@gmail.com>
2022-05-08 20:53:17 +0600

    update my items

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,16 +1,19 @@
-import React from "react";
-import Banner from "../components/Home/Banner/Banner";
-import Footer from "../Shared/Footer/Footer";
-import Header from "../Shared/Header/Header";
-
+import React from 'react';
+import Header from '../Shared/Header';
+import Footer from '../Shared/Footer';
+import Banner from '../Components/Home/Banner';
+import Services from '../Components/Home/Services';
+import Everything from '../Components/Home/Everything';
 const Home = () => {
-  return (
-    <div>
-      <Header></Header>
-      <Banner></Banner>
-      <Footer></Footer>
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+            <Banner />
+            <Services />
+            <Everything />
+            <Footer />
+        </div>
+    );
 };
 
 export default Home;
